422cf89320ad9627fb2979a8baed2c51e64103d1
Peter <ptrahrens@gmail.com>
2014-12-07 18:15:40 -0800

    recounting flops on nrm2 to be less optimistic

--- tests/common/test_vecvec_fill_header.h
+++ tests/common/test_vecvec_fill_header.h
@@ -11,13 +11,13 @@ int vecvec_fill_test(int argc, char** argv, int N, int incx, int incy, int type)
 
 const char* vecvec_name(int argc, char** argv){
   static char namebuf[NAME_SIZE];
-  int type = opt_read_int(argc, argv, "-t", 0);
+  int type = opt_read_int(argc, argv, "-t", vec_fill_RAND);
   snprintf(namebuf, NAME_SIZE * sizeof(char), "%s (%s)", vecvec_fill_name(argc, argv), vec_fill_name(type));
   return namebuf;
 }
 
 int vecvec_test(int argc, char** argv, int N, int incx, int incy){
-  int type = opt_read_int(argc, argv, "-t", 0);
+  int type = opt_read_int(argc, argv, "-t", vec_fill_RAND);
   int rc = vecvec_fill_test(argc, argv, N, incx, incy, type);
   return rc;
 }
