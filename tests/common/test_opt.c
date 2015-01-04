e6f0af5ad69a70e23c304c9ced221d9322423c00
Peter <ptrahrens@gmail.com>
2015-01-03 21:49:37 -0700

    working on new options

--- tests/common/test_opt.c
+++ tests/common/test_opt.c
@@ -3,15 +3,60 @@
 #include <stdlib.h>
 #include "test_opt.h"
 
-void opt_show_option(char* name, char* desc) {
-  printf("  %8s : %s\n", name, desc);
+void opt_show_option(opt_option option) {
+  opt_fprintf_option(stdout, opt_option option);
+}
+static void opt_fprintf_option(FILE *f, opt_option option) {
+  char* token;
+  token = strtok(option.header.help, "\n");
+  if(token){
+    fprintf(f, "-%c, --%8s: %64s\n", option.header.shortname, option.header.longname, token);
+    while(token){
+      fprintf(f, "                %64s\n", token);
+      token = strtok(NULL, "\n");
+    }
+  }else{
+    fprintf(f, "-%c, --%8s:", option.header.shortname, option.header.longname);
+  }
+  token = strtok(NULL, "\n");
+  fprintf(f, "                (");
+  if(option.header.required){
+    fprintf(f, "required ")
+  }
+  switch(option.type){
+    case opt_flag:
+      fprintf(f, "flag)\n");
+      break;
+    case opt_int:
+      fprintf(f, "int) values=[%d, %d] default=%d\n", option.i.min, option.i.max, option.i.default);
+      break;
+    case opt_string:
+      fprintf(f, "string) default=%d\n", option.s.default);
+      break;
+    case opt_double:
+      fprintf(f, "double) values=[%f, %f] default=%f\n", option.d.min, option.d.max, option.d.default);
+      break;
+    case opt_float:
+      fprintf(f, "float) values=[%f, %f] default=%f\n", option.f.min, option.f.max, option.f.default);
+      break;
+    case opt_named:
+      fprintf(f, "enum) values=[\n");
+      {
+        int i;
+        char[63] namedesc;
+        for(i = 0; i < option.n.n_names - 1; i++){
+          snprintf(namedesc, 63 * sizeof(char), "%s: %s", option.n.names[i], option.n.descs[i]);
+          fprintf(f, "                  %s\n", namedesc);
+        }
+        snprintf(namedesc, 62 * sizeof(char), "%s: %s", option.n.names[option.n.n_names - 1], option.n.descs[option.n.n_names - 1]);
+        fprintf(f, "                  %s]\n", namedesc);
+        fprintf(f, "                  default=%54s\n", option.n.names[option.n.default]);
+      }
+      break;
+  }
 }
 
-void opt_show_option_extended(char* desc) {
-  printf("           : %s\n", desc);
-}
-
-int opt_find_option( int argc, char **argv, const char *option )
+static int opt_find_flag(int argc, char **argv, char *flag)
 {
   int i;
   for( i = 1; i < argc; i++ )
@@ -20,73 +65,110 @@ int opt_find_option( int argc, char **argv, const char *option )
   return -1;
 }
 
-int opt_read_int( int argc, char **argv, const char *option, int default_value )
-{
-  int iplace = opt_find_option( argc, argv, option );
-  if( iplace >= 0 && iplace < argc-1 )
-    return atoi( argv[iplace+1] );
-  return default_value;
-}
-
-double opt_read_double( int argc, char **argv, const char *option, double default_value )
-{
-  int iplace = opt_find_option( argc, argv, option );
-  if( iplace >= 0 && iplace < argc-1 )
-    return atof( argv[iplace+1] );
-  return default_value;
-}
-
-float opt_read_float( int argc, char **argv, const char *option, float default_value )
-{
-  int iplace = opt_find_option( argc, argv, option );
-  if( iplace >= 0 && iplace < argc-1 )
-    return atof( argv[iplace+1] );
-  return default_value;
-}
-
-char *opt_read_string( int argc, char **argv, const char *option, char *default_value )
-{
-  int iplace = opt_find_option( argc, argv, option );
-  if( iplace >= 0 && iplace < argc-1 )
-    return argv[iplace+1];
-  return default_value;
-}
-
-int opt_read_irange(int argc, char **argv, const char *option,
-    int* pstart, int* pstop, int* pstep) {
-
-  int nbp = opt_find_option(argc, argv, option);
-  if (nbp < 0 || nbp + 1 >= argc) {
-    return  -1;
+void opt_eval_option(int argc, char **argv, opt_option *option);
+  char flag[11];
+  int i_flag;
+  snfprintf(f, flag, 11 * sizeof(str), "-%c", option->header.shortname);
+  i_flag = opt_find_flag(argc, argv, flag);
+  if(i_flag < 0){
+    snfprintf(f, flag, 11 * sizeof(str), "-%s", option->header.longname);
+    i_flag = opt_find_flag(argc, argv, flag);
   }
-  int start, stop, step;
-  char* nbs = argv[nbp + 1];
-  char* sep = NULL;
-  sep = strchr(nbs, ':');
-  if (sep == NULL) {
-    start = stop = atoi(nbs);
-    step = 1;
+  if(i_flag < 0 && option->header.required){
+    fprintf(stderr, "error: required argument not found\n");
+    opt_fprintf_option(stderr, *option);
+    exit(125);
   }
-  else {
-    sep[0] = 0;
-    start = atoi(nbs);
-    nbs = sep + 1;
-    sep = strchr(nbs, ':');
-    if (sep == NULL) {
-      stop = atoi(nbs);
-      step = 1;
-    }
-    else {
-      sep[0] = 0;
-      step = atoi(nbs);
-      nbs = sep + 1;
-      stop = atoi(nbs);
-    }
+  switch(option.type){
+    case opt_flag:
+      option.f.exists = i_flag >= 0;
+      break;
+    case opt_int:
+      if(i_flag >= 0){
+        if(i_flag + 1 >= argc){
+          fprintf(stderr, "error: option takes an argument\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        char *end;
+        option.i.value = (int)strtol(argv[i_flag + 1], &end, 0);
+        if(end != argv[i_flag + 1] + strlen(argv[i_flag + 1])){
+          fprintf(stderr, "error: invalid integer format %s\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        if(option.i.value > option.i.max || option.i.value < option.i.min){
+          fprintf(stderr, "error: integer out of bounds %d\n", option.i.value);
+        }
+      }else{
+        option.i.value = option.i.default;
+      }
+      break;
+    case opt_string:
+      if(i_flag >= 0){
+        if(i_flag + 1 >= argc){
+          fprintf(stderr, "error: option takes an argument\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        option.s.value = argv[i_flag + 1];
+      }else{
+        option.s.value = option.s.default;
+      }
+      break;
+    case opt_double:
+      if(i_flag >= 0){
+        if(i_flag + 1 >= argc){
+          fprintf(stderr, "error: option takes an argument\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        char *end;
+        option.d.value = strtod(argv[i_flag + 1], &end);
+        if(end != argv[i_flag + 1] + strlen(argv[i_flag + 1])){
+          fprintf(stderr, "error: invalid double format %s\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        if(option.d.value > option.d.max || option.d.value < option.d.min){
+          fprintf(stderr, "error: integer out of bounds %f\n", option.d.value);
+        }
+      }else{
+        option.d.value = option.d.default;
+      }
+      break;
+    case opt_float:
+      if(i_flag >= 0){
+        if(i_flag + 1 >= argc){
+          fprintf(stderr, "error: option takes an argument\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        char *end;
+        option.f.value = (float)strtod(argv[i_flag + 1], &end);
+        if(end != argv[i_flag + 1] + strlen(argv[i_flag + 1])){
+          fprintf(stderr, "error: invalid float format %s\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        if(option.f.value > option.f.max || option.f.value < option.f.min){
+          fprintf(stderr, "error: integer out of bounds %f\n", option.f.value);
+        }
+      }else{
+        option.d.value = option.d.default;
+      }
+      break;
+    case opt_named:
+      if(i_flag >= 0){
+        if(i_flag + 1 >= argc){
+          fprintf(stderr, "error: option takes an argument\n", argv[i_flag + 1]);
+          opt_fprintf_option(stderr, *option);
+        }
+        int i;
+        for(i = 0; i < option.n.n_names; i++){
+          if(strcmp(option.n.names[i], argv[i_flag + 1]) == 0){
+            option.n.value = i;
+            return;
+          }
+        }
+        fprintf(stderr, "error: invalid argument %s\n", argv[i_flag + 1]);
+        opt_fprintf_option(stderr, *option);
+      }else{
+        option.n.value = option.n.default;
+      }
+      break;
   }
-  if (stop < start) stop = start;
-  if (step < 1) step = 1;
-  *pstart = start;
-  *pstop = stop;
-  *pstep = step;
-  return 0;
 }
--- tests/common/test_opt.h
+++ tests/common/test_opt.h
@@ -1,16 +1,80 @@
-void opt_show_option( char* name, char* desc );
+#ifndef __TEST_OPT_H
+#define __TEST_OPT_H
 
-void opt_show_option_extended( char* desc );
+typedef enum opt_type{
+  opt_flag,
+  opt_int,
+  opt_string,
+  opt_double,
+  opt_float,
+  opt_named
+} opt_type;
 
-int opt_find_option( int argc, char **argv, const char *option );
+typedef struct opt_option_header{
+  opt_type    type;
+  char        short_name;
+  const char  *long_name;
+  const char  *help;
+  int         required;
+} opt_option_header;
 
-int opt_read_int( int argc, char **argv, const char *option, int default_value );
+typedef struct opt_option_flag{
+  opt_option_header header;
+  int               exists;
+} opt_option_flag;
 
-double opt_read_double( int argc, char **argv, const char *option, double default_value );
+typedef struct opt_option_int{
+  opt_option_header header;
+  int               min;
+  int               max;
+  int               default;
+  int               value;
+} opt_option_int;
 
-float opt_read_float( int argc, char **argv, const char *option, float default_value );
+typedef struct opt_option_string{
+  opt_option_header header;
+  const char        *default;
+  char              *value;
+} opt_option_string;
 
-char *opt_read_string( int argc, char **argv, const char *option, char *default_value );
+typedef struct opt_option_double{
+  opt_option_header header;
+  double            min;
+  double            max;
+  double            default;
+  double            value;
+} opt_option_double;
 
-int opt_read_irange( int argc, char **argv, const char *option,
-    int* pstart, int* pstop, int* pstep );
+typedef struct opt_option_float{
+  opt_option_header header;
+  float             min;
+  float             max;
+  float             default;
+  float             value;
+} opt_option_float;
+
+typedef struct opt_option_named{
+  opt_option_header header;
+  char              **names;
+  char              **descs;
+  int               n_names;
+  int               default;
+  int               value;
+} opt_option_named;
+
+typedef union opt_option{
+  opt_type          type;
+  opt_option_header header;
+  opt_option_flag   f;
+  opt_option_int    i;
+  opt_option_string s;
+  opt_option_double d;
+  opt_option_float  f;
+  opt_option_named  n;
+}
+
+void opt_show_option(opt_option option);
+
+void opt_eval_option(int argc, char **argv, opt_option *option);
+
+#endif
