a30eb79bc67dfcf1b95a148f0a4acd68c4776966
Peter <ptrahrens@gmail.com>
2015-03-04 20:17:12 -0800

    all generators looking good. now, time for an out of tree, supah build.

--- src/scnrm2I2.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "gen"))
-import generate
-import dataTypes
-import nrm2I2
-
-generate.generate(nrm2I2.Nrm2I2(dataTypes.FloatComplex), __file__, generate.get_tuning(__file__))
--- src/zdotcI2.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "gen"))
-import generate
-import dataTypes
-import dotcI2
-
-generate.generate(dotcI2.DotCI2(dataTypes.DoubleComplex), __file__, generate.get_tuning(__file__))
