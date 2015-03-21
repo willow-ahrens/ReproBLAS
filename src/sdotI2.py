9e62435d2bd609a91649c46f42b00d5991c576ac
Peter <ptrahrens@gmail.com>
2015-03-21 13:50:19 -0700

    Revert "Revert "Revert "Revert "all generators looking good. now, time for an out of tree, supah build.""""
    This reverts commit 68ac1453a66f2aa9b6b65a2dc1b96e17740ff6eb.

--- src/sdotI2.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "gen"))
-import generate
-import dataTypes
-import dotI2
-
-generate.generate(dotI2.DotI2(dataTypes.Float), __file__, generate.get_tuning(__file__))
