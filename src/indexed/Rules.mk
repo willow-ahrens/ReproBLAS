eadda2cfc675cd849a8c8431b3e2354e9f1f29ba
Peter <ptrahrens@gmail.com>
2015-04-22 00:49:40 -0700

    renaming

--- src/indexed/Rules.mk
+++ src/indexed/Rules.mk
@@ -5,4 +5,4 @@ INSTALL_LIB := $(TARGETS)
 
 LIBINDEXED := $(OBJPATH)/libindexed.a
 
-libindexed.a_DEPS = dconv.o ufp.o ufpf.o dbound.o dIAdd.o dIRenorm.o dIUpdate.o sbound.o sIAdd.o sconv.o sIRenorm.o sIUpdate.o diprint.o ziprint.o siprint.o ciprint.o
+libindexed.a_DEPS = dconv.o ufp.o ufpf.o dbound.o dIAdd.o dIRenorm.o dupdate.o sbound.o sIAdd.o sconv.o sIRenorm.o supdate.o diprint.o ziprint.o siprint.o ciprint.o
