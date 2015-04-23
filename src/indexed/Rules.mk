8a25ea8c583d1420dadb417930a04471158772a0
Peter <ptrahrens@gmail.com>
2015-04-22 17:46:39 -0700

    okay. no more touching the indexed library until parallel gemv is done.

--- src/indexed/Rules.mk
+++ src/indexed/Rules.mk
@@ -1,10 +1,8 @@
-TARGETS := libindexed.a addtest$(EXE)
-SUBDIRS := 
+TARGETS := libindexed.a
+SUBDIRS :=
 
 INSTALL_LIB := $(TARGETS)
 
 LIBINDEXED := $(OBJPATH)/libindexed.a
 
 libindexed.a_DEPS = dconv.o ufp.o ufpf.o dbound.o dIAdd.o dIRenorm.o dupdate.o sbound.o sIAdd.o sconv.o sIRenorm.o supdate.o diprint.o ziprint.o siprint.o ciprint.o
-
-addtest$(EXE)_DEPS = addtest.o $$(LIBINDEXED)
