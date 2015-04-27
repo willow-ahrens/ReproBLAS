3adec7a1e76c0ee0729743b22321926c629c076b
Peter <ptrahrens@gmail.com>
2015-04-27 13:59:29 -0700

    adding indexed memory functions.

---
+++ src/indexed/dimem.c
@@ -0,0 +1,67 @@
+#include "indexed.h"
+
+size_t disize(const int fold){
+  return 2*fold*sizeof(double);
+}
+
+size_t zisize(const int fold){
+  return 4*fold*sizeof(double);
+}
+
+size_t dinum(const int fold){
+  return 2*fold;
+}
+
+size_t zinum(const int fold){
+  return 4*fold;
+}
+
+double_indexed *dialloc(const int fold){
+  return (double_indexed*)malloc(disize(fold));
+}
+
+double_complex_indexed *zialloc(const int fold){
+  return (double_complex_indexed*)malloc(zisize(fold));
+}
+
+void dmset(const int fold, const double *repX, const int increpX, const double *carX, const int inccarX, const double *repY, const int increpY, const double carY, const int inccarY){
+  int i;
+  for(i = 0; i < fold; i++){
+    repY[i * increpY] = repX[i * increpX];
+    carY[i * inccarY] = carX[i * inccarX];
+  }
+}
+
+void diset(const int fold, const double_indexed *X, const double_indexed *Y){
+  memcpy(Y, X, disize(fold));
+}
+
+void zmset(const int fold, const double *repX, const int increpX, const double *carX, const int inccarX, const double *repY, const int increpY, const double carY, const int inccarY){
+  dmset(fold, repX, 2 * increpX, carX, 2 * inccarX, repY, 2 * increpY, carY, 2 * inccarY);
+  dmset(fold, repX + 1, 2 * increpX, carX + 1, 2 * inccarX, repY + 1, 2 * increpY, carY + 1, 2 * inccarY);
+}
+
+void ziset(const int fold, const double_complex_indexed *X, const double_complex_indexed *Y){
+  memcpy(Y, X, zisize(fold));
+}
+
+void dmsetzero(const int fold, const double *repX, const int increpX, const double carX, const int inccarX){
+  int i;
+  for(i = 0; i < fold; i++){
+    repX[i * increpX] = 0.0;
+    carX[i * inccarX] = 0.0;
+  }
+}
+
+void disetzero(const int fold, const double_indexed *X){
+  memset(X, 0, disize(fold));
+}
+
+void zmsetzero(const int fold, const double *repX, const int increpX, const double carX, const int inccarX){
+  dmsetzero(fold, repX, 2 * increpX, carX, 2 * inccarX);
+  dmsetzero(fold, repX + 1, 2 * increpX, carX + 1, 2 * inccarX);
+}
+
+void zisetzero(const int fold, const double_complex_indexed *X){
+  memset(X, 0, zisize(fold));
+}
---
+++ src/indexed/simem.c
@@ -0,0 +1,67 @@
+#include "indexed.h"
+
+size_t sisize(const int fold){
+  return 2*fold*sizeof(float);
+}
+
+size_t cisize(const int fold){
+  return 4*fold*sizeof(float);
+}
+
+size_t sinum(const int fold){
+  return 2*fold;
+}
+
+size_t cinum(const int fold){
+  return 4*fold;
+}
+
+float_indexed *sialloc(const int fold){
+  return (float_indexed*)malloc(sisize(fold));
+}
+
+float_complex_indexed *cialloc(const int fold){
+  return (float_complex_indexed*)malloc(cisize(fold));
+}
+
+void smset(const int fold, const float *repX, const int increpX, const float *carX, const int inccarX, const float *repY, const int increpY, const float carY, const int inccarY){
+  int i;
+  for(i = 0; i < fold; i++){
+    repY[i * increpY] = repX[i * increpX];
+    carY[i * inccarY] = carX[i * inccarX];
+  }
+}
+
+void siset(const int fold, const float_indexed *X, const float_indexed *Y){
+  memcpy(Y, X, sisize(fold));
+}
+
+void cmset(const int fold, const float *repX, const int increpX, const float *carX, const int inccarX, const float *repY, const int increpY, const float carY, const int inccarY){
+  smset(fold, repX, 2 * increpX, carX, 2 * inccarX, repY, 2 * increpY, carY, 2 * inccarY);
+  smset(fold, repX + 1, 2 * increpX, carX + 1, 2 * inccarX, repY + 1, 2 * increpY, carY + 1, 2 * inccarY);
+}
+
+void ciset(const int fold, const float_complex_indexed *X, const float_complex_indexed *Y){
+  memcpy(Y, X, cisize(fold));
+}
+
+void smsetzero(const int fold, const float *repX, const int increpX, const float carX, const int inccarX){
+  int i;
+  for(i = 0; i < fold; i++){
+    repX[i * increpX] = 0.0;
+    carX[i * inccarX] = 0.0;
+  }
+}
+
+void sisetzero(const int fold, const float_indexed *X){
+  memset(X, 0, sisize(fold));
+}
+
+void cmsetzero(const int fold, const float *repX, const int increpX, const float carX, const int inccarX){
+  dmsetzero(fold, repX, 2 * increpX, carX, 2 * inccarX);
+  dmsetzero(fold, repX + 1, 2 * increpX, carX + 1, 2 * inccarX);
+}
+
+void cisetzero(const int fold, const float_complex_indexed *X){
+  memset(X, 0, cisize(fold));
+}
