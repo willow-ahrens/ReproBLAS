ff3457564b3f66ea833168c6b98f9c0cd010fe30
Peter <ptrahrens@gmail.com>
2015-04-28 13:23:10 -0700

    needed the newline

--- src/indexed/diprint.c
+++ src/indexed/diprint.c
@@ -20,7 +20,7 @@ void dmprint(const int fold, double *repX, int increpX, double *carX, int inccar
   double M;
   for (i = 0; i < fold; i++, repX += increpX, carX += inccarX) {
     M = ufp(repX[0]);
-    printf("(2^%d: %g #%g =%g)", (int)log2(M), repX[0] - 1.5*M, carX[0], (carX[0] - 6) * 0.25 * M + repX[0]);
+    printf("(2^%d: %g #%g =%g)\n", (int)log2(M), repX[0] - 1.5*M, carX[0], (carX[0] - 6) * 0.25 * M + repX[0]);
   }
 }
 
