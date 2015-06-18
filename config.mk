bc99c6dc22c0036a3ab49206125eeec9ce54be82
Peter <ptrahrens@gmail.com>
2015-06-18 11:40:04 -0700

    getting ready to tune

--- config.mk
+++ config.mk
@@ -26,7 +26,7 @@ LDFLAGS +=
 #MPICFLAGS =
 
 # select python (comment all for auto)
-#PYTHON = python
+PYTHON = python
 #PYTHON = python3
 
 # select vectorization (comment all for auto)
@@ -43,7 +43,7 @@ LDFLAGS +=
 #SSE4_2 := true
 #SSE4_2 := false
 #AVX := true
-#AVX := false
+AVX := false
 #AVX2 := true
 #AVX2 := false
 
