599912351a6a25a889ef6e845534de98c810c424
Peter <ptrahrens@gmail.com>
2015-06-23 15:05:44 -0700

    report has overflow. common.h does not use magic numbers

--- src/common/common.h
+++ src/common/common.h
@@ -6,6 +6,7 @@
 #define COMMON_H_
 
 #include <stdint.h>
+#include <float.h>
 
 #ifndef MAX
 #define MAX(A,B) ((A)>(B)?(A):(B))
@@ -28,43 +29,43 @@ typedef union int_float_ {
 #define UFP(X) ({ \
   long_double tmp_UFP; \
   tmp_UFP.d = X; \
-  tmp_UFP.l &= 0x7FF0000000000000ull; \
+  tmp_UFP.l &= (2ull * DBL_MAX_EXP - 1) << (DBL_MANT_DIG - 1); \
   tmp_UFP.d; \
   })
 
 #define UFPF(X) ({ \
   int_float tmp_UFPF; \
   tmp_UFPF.f = X; \
-  tmp_UFPF.i &= 0x7F800000ul; \
+  tmp_UFPF.i &= (2ul * FLT_MAX_EXP - 1) << (FLT_MANT_DIG - 1); \
   tmp_UFPF.f; \
   })
 
 #define EXP(X) ({ \
   long_double tmp_EXP; \
   tmp_EXP.d = X; \
-  (int)((tmp_EXP.l >> 52) & 0x7FF);\
+  (int)((tmp_EXP.l >> (DBL_MANT_DIG - 1)) & (2 * DBL_MAX_EXP - 1));\
   })
 
-#define EXP_BIAS 0x3FE
+#define EXP_BIAS (DBL_MAX_EXP - 2)
 
 #define EXPF(X) ({ \
   int_float tmp_EXPF; \
   tmp_EXPF.f = X; \
-  (int)((tmp_EXPF.i >> 23) & 0xFF);\
+  (int)((tmp_EXPF.i >> (FLT_MANT_DIG - 1)) & (2 * FLT_MAX_EXP - 1));\
   })
 
-#define EXPF_BIAS 0x7E
+#define EXPF_BIAS (FLT_MAX_EXP - 2)
 
 #define ISNANINF(X) ({ \
   long_double tmp_ISNANINF; \
   tmp_ISNANINF.d = X; \
-  (tmp_ISNANINF.l & 0x7FF0000000000000ull) == 0x7FF0000000000000ull;\
+  (tmp_ISNANINF.l & ((2ull * DBL_MAX_EXP - 1) << (DBL_MANT_DIG - 1))) == ((2ull * DBL_MAX_EXP - 1) << (DBL_MANT_DIG - 1));\
   })
 
 #define ISNANINFF(X) ({ \
   int_float tmp_ISNANINFF; \
   tmp_ISNANINFF.f = X; \
-  (tmp_ISNANINFF.i & 0x7F800000ul) == 0x7F800000ul; \
+  (tmp_ISNANINFF.i & ((2ul * FLT_MAX_EXP - 1) << (FLT_MANT_DIG - 1))) == ((2ul * FLT_MAX_EXP - 1) << (FLT_MANT_DIG - 1)); \
   })
 
 #endif
