90e7232f8d82f9e07d00643d5b4beb7e5c99c933
Peter <ptrahrens@gmail.com>
2015-10-31 00:11:53 -0700

    added timing hooks. More thought could be put in here.

--- tests/benchs/report_bench.py
+++ tests/benchs/report_bench.py
@@ -11,16 +11,16 @@ bench_harness.add_suite(benchs.BenchSuite([benchs.BenchRDSUMTest(), benchs.Bench
 bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDSUMTest(), benchs.BenchRDSUMTest()], ["N", "fold", "FillX"], [[2**i for i in range(6, 13)], [fold], ["normal"]], "peak_time"))
 bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDSUMTest(), benchs.BenchRDSUMTest()], ["N", "fold", "FillX"], [[2**i for i in range(6, 13)], [fold], ["normal"]], "time"))
 
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDDOTTest(), benchs.BenchRDDOTTest()], ["N", "fold", "FillX"], [[2**i for i in range(6, 13)], [fold], ["normal"]], "peak_time"))
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDDOTTest(), benchs.BenchRDDOTTest()], ["N", "fold", "FillX"], [[2**i for i in range(6, 13)], [fold], ["normal"]], "time"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDDOTTest(), benchs.BenchRDDOTTest()], ["N", "fold", "FillX", "FillY"], [[2**i for i in range(6, 13)], [fold], ["normal"], ["normal"]], "peak_time"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDDOTTest(), benchs.BenchRDDOTTest()], ["N", "fold", "FillX", "FillY"], [[2**i for i in range(6, 13)], [fold], ["normal"], ["normal"]], "time"))
 
 trials = 10
 
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["NoTrans"], [3], ["normal"], [0]], "peak_time", silent_flags="--Order ColMajor"))
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["NoTrans"], [3], ["normal"], [trials]], "time", silent_flags="--Order ColMajor"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", "FillX", "FillY", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["NoTrans"], [3], ["normal"], ["constant"], ["constant"], [0]], "peak_time", silent_flags="--Order ColMajor"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", "FillX", "FillY", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["NoTrans"], [3], ["normal"], ["constant"], ["constant"], [trials]], "time", silent_flags="--Order ColMajor"))
 
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["Trans"], [3], ["normal"], [0]], "peak_time", silent_flags="--Order ColMajor"))
-bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["Trans"], [3], ["normal"], [trials]], "time", silent_flags="--Order ColMajor"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", "FillB", "FillC", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["Trans"], [3], ["normal"], ["constant"], ["constant"], [0]], "peak_time", silent_flags="--Order ColMajor"))
+bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMVTest(), benchs.BenchRDGEMVTest()], [("N", "M"), "TransA", "fold", "FillA", "FillB", "FillC", 'a'], [[(2**i, 2**i) for i in range(6, 13)], ["Trans"], [3], ["normal"], ["constant"], ["constant"], [trials]], "time", silent_flags="--Order ColMajor"))
 
 bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMMTest(), benchs.BenchRDGEMMTest()], [("N", "M", "K"), ("TransA", "TransB"), "fold", "FillA", 'a'], [[(2**i, 2**i, 2**i) for i in range(6, 13)], [("NoTrans", "NoTrans")], [3], ["normal"], [0]], "peak_time", silent_flags="--Order ColMajor"))
 bench_harness.add_suite(benchs.BenchSuite([benchs.BenchDGEMMTest(), benchs.BenchRDGEMMTest()], [("N", "M", "K"), ("TransA", "TransB"), "fold", "FillA", 'a'], [[(2**i, 2**i, 2**i) for i in range(6, 13)], [("NoTrans", "NoTrans")], [3], ["normal"], [trials]], "time", silent_flags="--Order ColMajor"))
