df5a4cad199e21369820972db918c66423369ebd
Peter <ptrahrens@gmail.com>
2016-03-03 21:55:35 -0800

    we have a new structure, and xblas tests are included

--- tests/common/Rules.mk
+++ tests/common/Rules.mk
@@ -1,9 +1,9 @@
 TARGETS := libtest.a test_data$(EXE)
-SUBDIRS := xblas
+SUBDIRS :=
 
 test_data$(EXE)_DEPS = libtest.a test_data.o
 test_data$(EXE)_LIBS = -lm
 
 LIBTEST := $(OBJPATH)/libtest.a
 
-libtest.a_DEPS = test_file.o test_opt.o test_time.o test_util.o test_metric.o $$(LIBTESTXBLAS)
+libtest.a_DEPS = test_file.o test_opt.o test_time.o test_util.o test_metric.o test_util_xblas_ddot_fill.o test_util_xblas_zdot_fill.o test_util_xblas_sdot_fill.o test_util_xblas_cdot_fill.o
